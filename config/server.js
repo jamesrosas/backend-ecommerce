f467bd44e4cd0f69f507a11fdc46ffa3a38be595
James Rosas <jamesadrian777@gmail.com>
2021-06-17 18:19:54 -0500

    empezando configuracion de strapi para deploy en heroku

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '95ffd67352432b39b42b1bc4c844afe8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a39dc0c0-6771-4618-a926-f39f96fc330a'
+};
