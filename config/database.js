51e812fa12da259914b6c95cbca009af3ffb4316
James Rosas <jamesadrian777@gmail.com>
2021-06-17 19:07:01 -0500

    actualizando configuracion de db

--- config/database.js
+++ config/database.js
@@ -1,11 +1,17 @@
-module.exports = ({ env }) => ({
+module.exports = () => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        // filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        host: `${process.env.DATABASE_HOST}`,
+        port: `${process.env.DATABASE_PORT}`,
+        database: `${process.env.DATABASE_NAME}`,
+        username: `${process.env.DATABASE_USERNAME}`,
+        password: `${process.env.DATABASE_PASSWORD}`,
+        ssl: true
       },
       options: {
         useNullAsDefault: true,
